8526bcbf689ab8e911fed2216d5c6f11cb2ad9e4
Austin Elwell <austinelwell99@gmail.com>
2021-03-31 21:36:34 -0500

    Cleaned up, handled edge case of no location

--- client/src/components/WebcamDisplay.jsx
+++ client/src/components/WebcamDisplay.jsx
@@ -8,16 +8,12 @@ const WebcamDisplay = ({ selectedWebcam }) => (
       title={selectedWebcam?.title}
       allowFullScreen={true}
     />
-    {/* <iframe
-      className='selectedWebcamVideo'
-      src={document.getElementById('main')?.children[0]?.src}
-      title={selectedWebcam?.title}
-      allowFullScreen={true}
-    /> */}
     <div className='selectedWebcamDetails'>
       <div className='selectedWebcamTitle'>{selectedWebcam?.title}</div>
       <div className='selectedWebcamCountryText'>
-        {selectedWebcam?.location?.country}
+        {selectedWebcam?.location?.region &&
+          selectedWebcam?.location?.country &&
+          `${selectedWebcam?.location?.region}, ${selectedWebcam?.location?.country}`}
       </div>
     </div>
   </div>
